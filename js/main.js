f8e88442ba3b09ad48300c4748489fc7c4b9f77e
Andrey Gaponik <andreygaponik@gmail.com>
2015-09-14 16:36:00 +0300

    add scrollbar

--- js/main.js
+++ js/main.js
@@ -7,19 +7,32 @@ $(document).ready(function() {
   $(function() {
   	$('.turn-aside').click(function(event) {
   		event.preventDefault();
-  		$('aside').animate({'left':'-20%'});
-  		$('.main-container').animate({
-  			'width':'100%',
-  			'marginLeft':'0'
-  		});
-  		$('.turn-aside').animate({'marginLeft':'20px'});
+  		
+  		if($(this).hasClass('aside-open')) {
+  			console.log('closed');
+  			$(this).removeClass('aside-open');
+
+  			$('aside').animate({'left':'-20%'});
+  			$('.main-container').animate({
+											  			'width':'100%',
+											  			'marginLeft':'0'
+											  		});
+  			$('.turn-aside').animate({'marginLeft':'20px'});
+
+  		} else {
+	  			console.log('open');
+	  			$(this).addClass('aside-open');
+	  			$('aside').animate({'left':'0'});
+
+	  			$('.main-container').animate({
+												  			'width':'80%',
+												  			'marginLeft':'20%'
+												  		});
+	  			$('.turn-aside').animate({'marginLeft':'0'});
+  			}
   	});
   });
 
-  $(function() {
-  	if (!$('aside').is('.aside-open')) {
-  		$('.main-container').addClass('width-100')
-  	} 
-  });
+
 
 });
